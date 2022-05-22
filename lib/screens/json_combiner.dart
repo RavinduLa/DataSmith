a6ebc858f6eae392a11f65d8387e7d9143d2a420
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-05-22 23:13:02 +0530

    Created basic layout for JSON combiner

--- lib/screens/json_combiner.dart
+++ lib/screens/json_combiner.dart
@@ -10,8 +10,42 @@ class JSONCombiner extends StatefulWidget {
 class _JSONCombinerState extends State<JSONCombiner> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('JSON Combiner'),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'JSON Combiner',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            Row(
+              children: [
+                const Text('First File'),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text('Select File'))
+              ],
+            ),
+            const Text('First File :  Not Selected'),
+            Row(
+              children: [
+                const Text('Second File'),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text('Select File'))
+              ],
+            ),
+            const Text('Second File :  Not Selected'),
+            Row(
+              children: [
+                const Text('Output File'),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text('Select Location'))
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
