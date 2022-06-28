cfdcbfcb2c8b1f15b0ffece151c0fb6ddc558f14
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-06-28 23:41:15 +0530

    Added json_combiner_fila_path_model.dart

---
+++ lib/models/json_combiner_fila_path_model.dart
@@ -0,0 +1,10 @@
+class JSONCombinerFilePathModel {
+  String firstFilePath;
+  String secondFilePath;
+  String outputPath;
+
+  JSONCombinerFilePathModel(
+      {required this.firstFilePath,
+      required this.secondFilePath,
+      required this.outputPath});
+}
